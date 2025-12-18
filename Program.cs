fbff0d188d1d5735b9ff7e1336fddea6f1ffb488
vamsi-lpu18 <vamsi14roll@gmail.com>
2025-12-18 10:31:46 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+ 
+class Area
+{
+    static void Main()
+    {
+        double r=Convert.ToDouble(Console.ReadLine());
+        double area=3.14*r*r;
+        Console.WriteLine(area);
+        // Console.Write("my name is vamsi");
+    }
+}
