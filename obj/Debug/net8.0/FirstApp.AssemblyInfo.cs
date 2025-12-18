7a4757f655c0dc094a56758d6cdb3534c8ec4040
vamsi-lpu18 <vamsi14roll@gmail.com>
2025-12-18 15:36:47 +0530

    Add Dec 18 task

--- obj/Debug/net8.0/FirstApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bbb5bac9ecb101c3ffd13ce48308446be417a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a562a1e8de5f40eb0fe837612fc9e61e6c66e5de")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
