cab5daf30f6cde03c3cadf7270cc06193563fc9a
vamsi-lpu18 <vamsi14roll@gmail.com>
2026-01-10 11:33:19 +0530

    Saturdays assingments

---
+++ Additional_questions/logistics_pro_shipment/Program.cs
@@ -0,0 +1,15 @@
+﻿class Shipment
+{
+    public string ShipmentCode{get;set;}
+    public string TransportMode{get;set;}
+    public double Weight{get;set;}
+    public int StorageDays{get;set;}
+}
+class ShipmentDetails:Shipment
+{
+    
+    public bool validateShipmentCode()
+    {
+        
+    }
+}
