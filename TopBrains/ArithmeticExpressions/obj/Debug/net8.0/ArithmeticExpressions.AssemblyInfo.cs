d32062efe0fb7b4917ed64e8c6ff5016cf7ce2a6
vamsi-lpu18 <vamsi14roll@gmail.com>
2026-02-04 15:22:58 +0530

    ineritance and polymorphism

--- TopBrains/ArithmeticExpressions/obj/Debug/net8.0/ArithmeticExpressions.AssemblyInfo.cs
+++ TopBrains/ArithmeticExpressions/obj/Debug/net8.0/ArithmeticExpressions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArithmeticExpressions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49eb108e635aeea32f28a23441242fd68722ac07")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArithmeticExpressions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArithmeticExpressions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
